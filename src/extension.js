ae3a7cccec4903a3a5c491840ea84857ebf22805
Santhosh <pitakasanthosh@gmail.com>
2024-01-28 17:09:47 +0530

    Add user profile functionality
    Add webview and styles for user profile
    Add user profile functionality to extension.js

--- src/extension.js
+++ src/extension.js
@@ -1,24 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+const userProfile = require("./pages/profile.js");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log(
     'Congratulations, your extension "hashnode-for-vscode" is now active!'
   );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   context.subscriptions.push(
     vscode.commands.registerCommand("hashnode-for-vscode.setUserName", () => {
       vscode.window
@@ -30,10 +16,13 @@ function activate(context) {
           vscode.window.showInformationMessage(
             "Welcome" + name + "to Hashnode for VSCode"
           );
-        })
-        .catch((err) => {
-          vscode.window.showErrorMessage("Unable to set Username,Try Again");
+          userProfile(context);
         });
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("hashnode-for-vscode.userProfile", () => {
+      userProfile(context);
     })
   );
 }
---
+++ src/pages/profile.js
@@ -0,0 +1,108 @@
+const getUris = require("../helpers/getUris.js");
+const vscode = require("vscode");
+const userQuery = require("../apiQueries/user.js");
+
+function userProfile(context) {
+  const username = context.globalState.get("hashnodeUserName");
+  if (username) {
+    const panel = vscode.window.createWebviewPanel(
+      "hashnodeProfile",
+      "Hashnode Profile",
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+      }
+    );
+
+    const {
+ userStyleSheet, createProfileScript, hashnodeImg, githubIcon, linkedinIcon, twitterIcon, websiteIcon, facebookIcon, instagramIcon, stackoverflowIcon, youtubeIcon, calendarIcon, locationIcon,
+    } = getUris(context);
+    const userStyleSheetUri = panel.webview.asWebviewUri(userStyleSheet);
+    const createProfileScriptUri = panel.webview.asWebviewUri(createProfileScript);
+    const hashnodeImgUri = panel.webview.asWebviewUri(hashnodeImg);
+    
+    // Create WebView URIs for the remaining icons
+    const githubIconUri = panel.webview.asWebviewUri(githubIcon);
+    const linkedinIconUri = panel.webview.asWebviewUri(linkedinIcon);
+    const twitterIconUri = panel.webview.asWebviewUri(twitterIcon);
+    const websiteIconUri = panel.webview.asWebviewUri(websiteIcon);
+    const facebookIconUri = panel.webview.asWebviewUri(facebookIcon);
+    const instagramIconUri = panel.webview.asWebviewUri(instagramIcon);
+    const stackoverflowIconUri = panel.webview.asWebviewUri(stackoverflowIcon);
+    const youtubeIconUri = panel.webview.asWebviewUri(youtubeIcon);
+    const calendarIconUri = panel.webview.asWebviewUri(calendarIcon);
+    const locationIconUri = panel.webview.asWebviewUri(locationIcon);
+
+
+    panel.webview.html = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hashnode for VS Code</title>
+    <link rel="stylesheet" href="${userStyleSheetUri}">
+</head>
+<body>
+    <header>
+       <div class="header-wrapper">
+       <img src="${hashnodeImgUri}" alt="hashnode-icon" class="hashnode-icon" width="100"/>
+        <h1>Hashnode for VS Code</h1>
+       </div>
+    </header>
+    <main>
+        <div class="profile-wrapper">
+        <h2>Profile</h2>
+        </div>
+    </main>
+    <footer></footer>
+    <script src="${createProfileScriptUri}"></script>
+    <script>
+    fetch("https://gql.hashnode.com", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    query: \`
+    ${userQuery}
+    \`,
+    variables: {
+      username: "${username}",
+      roles: ["OWNER"]
+    }
+  })
+})
+.then((res) => res.json())
+.then((data) => {
+  const profile = data.data.user;
+  const icons = {
+    githubIcon: "${githubIconUri}",
+    twitterIcon: "${twitterIconUri}",
+    linkedinIcon: "${linkedinIconUri}",
+    websiteIcon: "${websiteIconUri}",
+    facebookIcon: "${facebookIconUri}",
+    instagramIcon: "${instagramIconUri}",
+    stackoverflowIcon: "${stackoverflowIconUri}",
+    youtubeIcon: "${youtubeIconUri}",
+    calendarIcon: "${calendarIconUri}",
+    locationIcon: "${locationIconUri}"
+  };
+  const profileWrapper = document.querySelector(".profile-wrapper");
+  const profileHTML = createProfile(profile,icons);
+  profileWrapper.innerHTML = profileHTML;
+})
+.catch((error) => console.error('Error:', error));
+
+    </script>
+</body>
+</html>
+    `;
+  } else {
+    vscode.window.showInformationMessage(
+      "Please set your Hashnode username first"
+    );
+  }
+}
+
+module.exports = userProfile;
